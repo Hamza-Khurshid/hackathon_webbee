78e5ea0dd798bac350394dae1fb12bafc31f2439
hamza-khurshid <hamzakhurshid5509@gmail.com>
2023-05-27 18:15:49 +0500

    textinput added

--- src/screens/categoryItems/item.tsx
+++ src/screens/categoryItems/item.tsx
@@ -1,19 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
+import { colors } from '../../theme/colors'
 
 const Item = () => {
   return (
     <View>
-      <Text>Item</Text>
-      <Text>Item</Text>
-      <Text>Item</Text>
-      <Text>Item</Text>
-      <Text>Item</Text>
-      <Text>Item</Text>
+      <TextInput style={styles.input} placeholder='Enter your name here' />
+      <TextInput style={styles.input} placeholder='Enter your name here' />
+      <TextInput style={styles.input} placeholder='Enter your name here' />
+      <TextInput style={styles.input} placeholder='Enter your name here' />
     </View>
   )
 }
 
 export default Item
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: '100%',
+    backgroundColor: colors.white,
+    paddingHorizontal: 10,
+    marginTop: 10
+  }
+})
